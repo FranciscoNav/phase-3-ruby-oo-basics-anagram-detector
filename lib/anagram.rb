7e123ed0b8dd97ab8f1bd30c4c32027b6aa5edf9
Francisco <fnaveira11@gmail.com>
2021-05-18 13:14:00 -0400

    commit tree 2a6640182b40826280ab8a70cc01ac72f349b6da

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+
+    def match(array)
+        # binding.pry
+        array.select {|word| word.split("").sort == @word.split("").sort}
+
+    end
+end
